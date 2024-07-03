2c265cb134c92a0381447b139899724fd7675d2e
Homvi <honvedoadamkj@gmail.com>
2024-07-03 13:29:25 +0200

    Use Middleware on a root Route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ExpressionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\InjectLocaleData;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -18,7 +19,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home');
-});
+})->middleware(InjectLocaleData::class);
 
 
 
