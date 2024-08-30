8c1238816cd6f7eb8ccb6febd71d76f7c3379b16
Homvi <honvedoadamkj@gmail.com>
2024-08-30 12:29:34 +0200

    Create ExpressionContainer Coponent

---
+++ resources/js/Components/ExpressionsContainer.jsx
@@ -0,0 +1,16 @@
+const ExpressionsContainer = ({ fadeIn, children }) => {
+    return (
+        <div
+            id="expressionsContainer"
+            className={`flex flex-col text-center justify-center w-full md:max-w-xl items-center gap-3 transition-all duration-300 ${
+                fadeIn
+                    ? "opacity-0 translate-x-full"
+                    : "opacity-100 translate-x-0"
+            }`}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default ExpressionsContainer;
