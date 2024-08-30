c051ea1ecaf29df0bddbae454d2bcde2ff759a09
Homvi <honvedoadamkj@gmail.com>
2024-08-30 21:45:05 +0200

    Refactor: ChooseGameModeController to group and count expressions by language pair using Eloquent collections

--- app/Http/Controllers/ChooseGameModeController.php
+++ app/Http/Controllers/ChooseGameModeController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expression;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ChooseGameModeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        return Inertia::render('ChooseGameMode');
+        // Get the count of expressions per language pair
+        $expressionCounts = Expression::where('is_validated', true)
+            ->get()
+            ->groupBy(function ($item) {
+                return "{$item->expression_language}-{$item->answer_language}";
+            })
+            ->map->count();
+
+        // Pass the counts to the frontend
+        return Inertia::render('ChooseGameMode', [
+            'expressionCounts' => $expressionCounts
+        ]);
     }
 }
