c734b7ed4f1838343d64cdc1a8afe4b98327b3c1
Homvi <honvedoadamkj@gmail.com>
2024-08-30 14:24:30 +0200

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ export default {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                midnight: "#052138",
+            },
         },
     },
 
