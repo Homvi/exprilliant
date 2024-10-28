496981a1ed343d747a253c710ef3be988fc229fc
Adam <honvedoadamkj@gmail.com>
2024-10-28 10:44:12 +0100

    pass user experience and name

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class GameController extends Controller
 {
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        return Inertia::render('Game');
+        // Get users with experience > 0, sorted by experience in descending order
+        $users = User::where('experience', '>', 0)
+            ->orderBy('experience', 'desc')
+            ->get(['name', 'experience']);  // Fetch only the name and experience fields
+
+
+        return Inertia::render('Game', [
+            'users' => $users, // Pass the users to the Toplist component
+        ]);
     }
 }
