d7092668d678f297d752f363e7143cf1500bdf9b
Adam <honvedoadamkj@gmail.com>
2024-10-30 09:52:23 +0100

    refactor: update Expression interface to include User object and change property order

--- resources/js/Types/Expressions.ts
+++ resources/js/Types/Expressions.ts
@@ -1,14 +1,16 @@
+import { User } from './User';
+
 export interface Expression {
-  id: number;
+  answer_language: string;
+  created_at: string;
+  example_usage: string | null;
   expression: string;
-  right_answer: string;
+  expression_language: string;
   false_answer_one: string;
   false_answer_two: string;
-  expression_language: string;
-  answer_language: string;
-  example_usage: string | null;
-  user_id: number;
-  created_at: string;
-  updated_at: string;
+  id: number;
   is_validated: number;
+  right_answer: string;
+  user: User;
+  updated_at: string;
 }
