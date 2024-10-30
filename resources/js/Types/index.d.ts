e0a24509676247e8f1b44e6c3b954bc7b7b160f1
Adam <honvedoadamkj@gmail.com>
2024-10-30 10:09:02 +0100

    feat: update User interface to include experience, created_at, and updated_at fields

--- resources/js/Types/index.d.ts
+++ resources/js/Types/index.d.ts
@@ -1,8 +1,11 @@
 export interface User {
-  id: number;
   name: string;
+  id: number;
+  experience: number;
   email: string;
   email_verified_at?: string;
+  created_at: string;
+  updated_at: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
