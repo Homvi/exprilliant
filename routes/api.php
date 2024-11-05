ac2685286f87babe2d896556b83f40e6c93eafd2
Adam <honvedoadamkj@gmail.com>
2024-11-05 14:49:39 +0100

    feat(api): add route for fetching toplist data using ToplistController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ToplistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API route for fetching toplist data as JSON
+Route::get('/toplist', [ToplistController::class, 'getToplistData'])->name('api.toplist');
