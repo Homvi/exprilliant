b20f7b07bbe256e0e9adb0232f991e289a4b1bb2
Adam <honvedoadamkj@gmail.com>
2024-11-05 14:47:51 +0100

    refactor(game): simplify component by removing users prop and adding experience update logic

--- resources/js/Pages/Game.tsx
+++ resources/js/Pages/Game.tsx
@@ -4,26 +4,40 @@ import CustomGuestLayout from '../Layouts/CustomGuestLayout';
 import GameInProgress from '@/Components/GameInProgress';
 import { useGameMode } from '@/contexts/GameModeContext';
 import { useGameStore } from '@/store/gameStore';
-import { useFetchExpressions } from '@/hooks/useFetchExpressions';
-import { User } from '@/types';
+import { useExpressions } from '@/hooks/useExpressions';
 import { numberOfExpressions } from '@/config';
+import { useEffect } from 'react';
+import { updateExperience } from '@/functions/gameHelpers';
+import { useToplistStore } from '@/store/toplistStore';
 
-const Game: React.FC<{ users: User[] }> = ({ users }) => {
+const Game = () => {
   const { gameMode } = useGameMode();
-  const { isGameFinished } = useGameStore();
+  const { isGameFinished, score } = useGameStore();
+  const { fetchToplistData } = useToplistStore();
 
   if (gameMode) {
-    useFetchExpressions(gameMode, numberOfExpressions);
+    useExpressions(gameMode, numberOfExpressions);
   } else {
     window.location.href = '/choose-game-mode';
   }
+
+  useEffect(() => {
+    const updateScoreAndFetchData = async () => {
+      if (isGameFinished && score > 0) {
+        await updateExperience(score);
+        await fetchToplistData();
+      }
+    };
+
+    updateScoreAndFetchData();
+  }, [isGameFinished]);
 
   return (
     <>
       <Head title="Game" />
       <CustomGuestLayout>
         <div className="h-full flex flex-col items-center gap-3 mt-3 mx-3 font-nova">
-          {isGameFinished ? <Score users={users} /> : <GameInProgress />}
+          {isGameFinished ? <Score /> : <GameInProgress />}
         </div>
       </CustomGuestLayout>
     </>
