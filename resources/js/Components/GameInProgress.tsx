45993bfdf598204a72c3d6e955bac2f10857496f
Adam <honvedoadamkj@gmail.com>
2024-11-05 11:57:25 +0100

    feat: add answer selection and highlighting logic

--- resources/js/Components/GameInProgress.tsx
+++ resources/js/Components/GameInProgress.tsx
@@ -1,4 +1,3 @@
-// GameInProgress.tsx
 import ProgressBar from '@/Components/ProgressBar';
 import ExpressionsContainer from '@/Components/ExpressionsContainer';
 import ActiveExpressionHeader from '@/Components/ActiveExpressionHeader';
@@ -8,17 +7,49 @@ import { useGameStore } from '@/store/gameStore';
 import { useGameProgress } from '@/hooks/useGameProgress';
 import { ActiveExpressionChoiceType } from '@/types/Expressions';
 import { getChoicesInShuffledOrder } from '@/functions/gameHelpers';
+import { useEffect, useState } from 'react';
 
 const GameInProgress: React.FC = () => {
   const { expressions, activeExpressionIndex } = useGameStore();
   const { handleChoice } = useGameProgress();
 
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+  const [isBlocked, setIsBlocked] = useState(false);
+  const [activeExpressionChoices, setActiveExpressionChoices] = useState<ActiveExpressionChoiceType[]>([]);
+
+  useEffect(() => {
+    const activeExpressionChoices = getChoicesInShuffledOrder(expressions[activeExpressionIndex]);
+    setActiveExpressionChoices(activeExpressionChoices);
+  }, [activeExpressionIndex, expressions]);
+
+  useEffect(() => {
+    if (selectedAnswer) {
+      highlightChoices(selectedAnswer);
+    }
+  }, [selectedAnswer]);
+
+  function handleSelectAnswer(choice: string): void {
+    if (isBlocked) return; // Prevent double selection
+    setIsBlocked(true);
+    setSelectedAnswer(choice);
+    setTimeout(() => {
+      handleChoice(choice);
+      setIsBlocked(false);
+      setSelectedAnswer(null);
+    }, 1000);
+  }
+
+  function highlightChoices(answerChosen: string) {
+    setActiveExpressionChoices((prevChoices) =>
+      prevChoices.map((choice) =>
+        choice.answer === answerChosen || choice.correct ? { ...choice, highlight: true } : choice
+      )
+    );
+  }
+
   const activeExpression = expressions[activeExpressionIndex];
 
   if (!activeExpression) return null;
-
-  // Generate choices using getChoicesInShuffledOrder
-  const activeExpressionChoices: ActiveExpressionChoiceType[] = getChoicesInShuffledOrder(activeExpression);
 
   return (
     <>
@@ -28,8 +59,10 @@ const GameInProgress: React.FC = () => {
         <div className="flex flex-col gap-3">
           {activeExpressionChoices.map((choice, id) => (
             <Choice
+              isHighlighted={choice.highlight}
+              isBlocked={isBlocked}
               key={`${id}_${choice.answer}`}
-              handleSelect={handleChoice}
+              handleSelect={handleSelectAnswer}
               isCorrect={choice.correct}
               {...choice}
               content={choice.answer}
