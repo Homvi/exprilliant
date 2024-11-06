85330c2a6f61297a5c66b394e019e8884fc10ccd
Adam <honvedoadamkj@gmail.com>
2024-11-06 08:34:29 +0100

    refactor: update Navbar to integrate BurgerMenu for streamlined navigation

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link, usePage } from '@inertiajs/react';
 import logo from '../../assets/exprilliant-with-text.webp';
 import axios from 'axios';
 import { LocalizedText } from '@/types/locale';
+import BurgerMenu from './BurgerMenu';
 
 const Navbar = () => {
   const { localeData, auth } = usePage<{ localeData: { data: LocalizedText }; auth: any }>().props;
@@ -23,33 +24,7 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="flex-none">
-        <ul className={`menu menu-horizontal px-1 ${isFontSizeLarge ? 'text-xl' : ''}`}>
-          {!auth.user && (
-            <>
-              <li>
-                <Link href="/register">{navbar.register}</Link>
-              </li>
-              <li>
-                <Link href="/login">{navbar.login_nav}</Link>
-              </li>
-            </>
-          )}
-          {auth.user && (
-            <li onClick={handleLogout}>
-              <a href="#">{navbar.logout}</a>
-            </li>
-          )}
-          {auth.user && (
-            <li>
-              <Link href="/request-new-expression">{navbar.request_new_expression}</Link>
-            </li>
-          )}
-          {auth.user?.email === 'adam.honvedo@gmail.com' && (
-            <li>
-              <Link href="/admin/unvalidated-expressions">Validate expressions</Link>
-            </li>
-          )}
-        </ul>
+        <BurgerMenu />
       </div>
     </div>
   );
