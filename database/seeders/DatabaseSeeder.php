9df31d0fd3cfb167f6f2ef6c36eaae24a24f7806
Homvi <honvedoadamkj@gmail.com>
2025-08-17 17:25:11 +0200

    refactor: adjust seeder order in DatabaseSeeder
    - Updated the DatabaseSeeder to call TestUserSeeder before ExpressionSeeder for improved data integrity during seeding.
    - This change ensures that test users are created prior to seeding expressions, facilitating better testing scenarios.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // Seed expressions from JSON file
+        // Seed test users first, then expressions
         $this->call([
+            TestUserSeeder::class,
             ExpressionSeeder::class,
         ]);
     }
